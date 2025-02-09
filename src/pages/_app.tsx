17592105ea6db87c58104d8c9d27cc43118036cd
ardiansetya <ardiansetya129@gmail.com>
2025-02-09 19:46:57 +0700

    wlowlwolwo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,4 @@
 import { AppSidebar } from "@/components/app-sidebar";
-import { Header } from "@/components/Header";
 import { SidebarProvider } from "@/components/ui/sidebar";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
